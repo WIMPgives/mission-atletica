7220e61b1067193b8d80bb6519f71af2008ab26e
Cole Geissinger <colegeissinger@gmail.com>
2014-11-01 15:18:01 -0700

    fixing bad PHP and code alignments

--- sidebar.php
+++ sidebar.php
@@ -20,15 +20,15 @@
 	</div>
 	<div class="row video"><img src="http://bestcamera.biz/wp-content/uploads/2014/08/video-player-button.png" width="80%" height="80%;"></div>
 	<div class="row donate">
-	<img src="http://cdn.flaticon.com/png/256/38703.png" width="100px" height="100px">
-	<h4>donate your shoes</h4>
-	<p>shoes needed 9999/9999</p>
-	<p>shoes donated 9999/9999</p>
-	<div class="progress">
-	 	<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 30%;">
-		30%
-	 	</div>
-	</div>
+		<img src="http://cdn.flaticon.com/png/256/38703.png" width="100px" height="100px">
+		<h4>donate your shoes</h4>
+		<p>shoes needed 9999/9999</p>
+		<p>shoes donated 9999/9999</p>
+		<div class="progress">
+		    <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 30%;">
+			30%
+		    </div>
+		</div>
 	</div>
 	<div class="row"><span class="btn btn-primary"> Retailers go here</span></div>
 </div>
