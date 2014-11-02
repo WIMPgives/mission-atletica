c5ddb704f634dbbf8f3ea718e40a4c1fd2911beb
Cole Geissinger <colegeissinger@gmail.com>
2014-11-01 17:53:29 -0700

    Fixing footer alignment

--- footer.php
+++ footer.php
@@ -12,14 +12,16 @@
 </div>
 		<div class="row footer">
 			<div class="container">
-				<div class="col-md-8">
-					<ul>
-						<li>Providing running shoes and running races for the youth of Guatemala.</li>
-						<li><a href="/">Mission Atletica</a> is a 501C(3) non-profit organization. Marin Link Inc. serves as the fiscal sponsor.</li>
-					</ul>
-				</div>
-				<div class="col-md-4">
-					<p>&copy; <a href="/">Mission Atletica</a>, <?= date("Y"); ?>. All rights reserved.</p>
+				<div class="row">
+					<div class="col-md-8">
+						<ul class="left-footer">
+							<li>Providing running shoes and running races for the youth of Guatemala.</li>
+							<li><a href="/">Mission Atletica</a> is a 501C(3) non-profit organization. Marin Link Inc. serves as the fiscal sponsor.</li>
+						</ul>
+					</div>
+					<div class="col-md-4 right-footer">
+						<p>&copy; <a href="/">Mission Atletica</a>, <?= date("Y"); ?>. All rights reserved.</p>
+					</div>
 				</div>
 			</div>
 		</div>
