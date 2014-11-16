4cb8c1f99b2dde416309cee60cc463dda6d5d4a2
Cole Geissinger <colegeissinger@gmail.com>
2014-11-16 14:56:08 -0800

    Creating a single page template for the sponsors pages

---
+++ single-sponsors.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @package Mission Atletica
+ */
+
+get_header(); ?>
+
+<div class="<?php echo esc_attr( mission_atletica_get_col_widths() ); ?>">
+
+	<div class="row  content">
+		<div class="col-md-12">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+
+			<?php endwhile; // end of the loop. ?>
+
+		</div>
+	</div>
+</div>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
